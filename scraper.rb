0e31aaf8bd6781e02710bafa86e147776fc8df0a
Tony Bowden <tony@tmtm.com>
2017-04-28 14:01:17 +0100

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -9,7 +10,7 @@ require 'open-uri'
 
 # thai_senate URL gives photos and senate numerical IDs
 # note "{PAGE-NUMBER}" placemarker will be replaced on the fly
-thai_senate_url = "http://www.senate.go.th/w3c/senate/senator.php?id=18&page={PAGE-NUMBER}&orby=&orrg=ASC"
+thai_senate_url = 'http://www.senate.go.th/w3c/senate/senator.php?id=18&page={PAGE-NUMBER}&orby=&orrg=ASC'
 
 # Wikipedia has kindly separated out the honorifics
 wikipedia_url = URI.encode('https://th.wikipedia.org/wiki/สภานิติบัญญัติแห่งชาติ_(ประเทศไทย)_พ.ศ._2557')
@@ -25,7 +26,7 @@ $thai_term = '2557'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -34,7 +35,7 @@ def noko_for(url)
 end
 
 def get_senate_url(url, page_number)
-  return url.sub('{PAGE-NUMBER}', "#{page_number}")
+  url.sub('{PAGE-NUMBER}', page_number.to_s)
 end
 
 # Thai honorifics are found by scraping wikipedia's entry for the senate.
@@ -44,16 +45,16 @@ end
 def scrape_wiki_list_for_honorifics(url)
   noko = noko_for(url)
   honorifics = []
-  thai_id=".E0.B8.81.E0.B8.B2.E0.B8.A3.E0.B9.81.E0.B8.95.E0.B9.88.E0.B8.87.E0.B8.95.E0.B8.B1.E0.B9.89.E0.B8.87.E0.B8.A3.E0.B8.AD.E0.B8.9A.E0.B9.81.E0.B8.A3.E0.B8.81"
-  noko.xpath(%Q{//h3[span[@id="#{thai_id}"]]/following-sibling::table[1]//ol/li[a]}).each do |li|
-    honorifics << li.xpath('./text()[not(preceding-sibling::a)]').text().tidy
+  thai_id = '.E0.B8.81.E0.B8.B2.E0.B8.A3.E0.B9.81.E0.B8.95.E0.B9.88.E0.B8.87.E0.B8.95.E0.B8.B1.E0.B9.89.E0.B8.87.E0.B8.A3.E0.B8.AD.E0.B8.9A.E0.B9.81.E0.B8.A3.E0.B8.81'
+  noko.xpath(%(//h3[span[@id="#{thai_id}"]]/following-sibling::table[1]//ol/li[a])).each do |li|
+    honorifics << li.xpath('./text()[not(preceding-sibling::a)]').text.tidy
     # we're ignoring name because we get them from the Senate's own site,
     # name = li.css('a[1]')a.text
     # but title is handy for wikinames, so store that separately
     title = li.xpath('a[not(@class="new")]/@title').text
-    ScraperWiki.save_sqlite([:name], {name: title}, 'wikinames')
+    ScraperWiki.save_sqlite([:name], { name: title }, 'wikinames')
   end
-  return honorifics.uniq.sort_by{|x| x.length}.reverse
+  honorifics.uniq.sort_by(&:length).reverse
 end
 
 def split_honorific_and_name(raw_name, honorifics)
@@ -65,7 +66,7 @@ def split_honorific_and_name(raw_name, honorifics)
       break
     end
   end
-  return [name.tidy, honorific]
+  [name.tidy, honorific]
 end
 
 def scrape_senate_page(url, page_number, honorifics)
@@ -76,24 +77,23 @@ def scrape_senate_page(url, page_number, honorifics)
   noko.xpath('//div[@id="maincontent"]//table[1]/tr[td]').each do |tr|
     tds = tr.css('td')
     senate_id = tds[0].text.tidy
-    if senate_id.match(/^\d+$/)
-      raw_name = tds[2].text.tidy
-      name, honorific = split_honorific_and_name(raw_name, honorifics)
-      image_url = tds[1].xpath('./img/@src').text.tidy
-      # TODO @tmtmtm suggests using
-      # TODO something like image[/(\d+).JPG/, 1] for id
-      data = { 
-        id: senate_id,
-        name: name,
-        image: image_url,
-        honorific_prefix: honorific,
-        party: $thai_party,
-        term: $thai_term,
-        source: url
-      }
-      ScraperWiki.save_sqlite([:id], data)
-      qty_members+=1
-    end
+    next unless senate_id =~ /^\d+$/
+    raw_name = tds[2].text.tidy
+    name, honorific = split_honorific_and_name(raw_name, honorifics)
+    image_url = tds[1].xpath('./img/@src').text.tidy
+    # TODO: @tmtmtm suggests using
+    # TODO something like image[/(\d+).JPG/, 1] for id
+    data = {
+      id:               senate_id,
+      name:             name,
+      image:            image_url,
+      honorific_prefix: honorific,
+      party:            $thai_party,
+      term:             $thai_term,
+      source:           url,
+    }
+    ScraperWiki.save_sqlite([:id], data)
+    qty_members += 1
   end
   puts "    members on this page: #{qty_members}"
 end
@@ -103,9 +103,9 @@ def get_number_of_senate_pages(url)
   puts "--> get_number_of_senate_pages(#{url})"
   noko = noko_for(url)
   page_menu = noko.xpath("//*[text()[contains(.,'หน้า')]]")
-  last_page_number = page_menu.xpath('./a[last()]').text().to_i
+  last_page_number = page_menu.xpath('./a[last()]').text.to_i
   puts "    last page number: #{last_page_number}"
-  return last_page_number
+  last_page_number
 end
 
 number_of_senate_pages = get_number_of_senate_pages(thai_senate_url)
